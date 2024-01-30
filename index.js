8dcafe5249461ccd7b97e7758303c0f6b9a0f4db
llatifah99 <litalatifah6@gmail.com>
2024-01-30 13:54:44 +0700

    artikel

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3030;
+const functions = require("firebase-functions");
 
 const { errorHandler } = require("./middleware");
 const articleRoutes = require("./routes/articleRoutes");
@@ -12,3 +13,5 @@ app.use(errorHandler);
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+exports.api = functions.https.onRequest(app);
